c208e680ddfa5642f7a74aed372919337cf716a7
Casper Jarhult <casper.jarhult@gmail.com>
2025-01-14 14:57:45 +0100

    Minor updates

--- dags/staging_dag.py
+++ dags/staging_dag.py
@@ -30,35 +30,52 @@ HEADERS = {
     "Authorization": f"Bearer {API_KEY}"
 }
 
-# Split the name into first and last names
+def clean_data(output_folder: str):
+    df = pd.read_csv(f'{output_folder}/raw_player_data.csv')
+
+    # Remove unnecessary columns
+    df_cleaned = df.iloc[:, [0, 2]]
+
+    # Remove rows with missing birthdates
+    df_cleaned = df_cleaned.dropna(subset=[df_cleaned.columns[1]])
+    df_cleaned.to_csv(f'{output_folder}/player_data.csv', index=False)
+
+clean_data = PythonOperator(
+    task_id='clean_data_task',
+    python_callable=clean_data,
+    op_kwargs={"output_folder": "/opt/airflow/data"},
+    dag=dag,
+)
+
 def split_names(name):
     parts = name.split()
     first_name = parts[0]
     last_name = " ".join(parts[1:]) if len(parts) > 1 else ""
     return first_name, last_name
 
-# Remove quotes from names
 def clean_names(name):
     return re.sub(r'\s"[^"]+"\s', ' ', name).strip()
 
-def process_csv(output_folder: str):
-    df = pd.read_csv(f'{output_folder}/raw_player_data.csv')
-
-    df_filtered = df.iloc[:, [0, 2]].copy()
-    df_filtered = df_filtered.dropna(subset=[df_filtered.columns[1]])
-    df_filtered.iloc[:, 0] = df_filtered.iloc[:, 0].apply(clean_names)
-
-    df_split = df_filtered.iloc[:, 0].apply(lambda x: pd.Series(split_names(x)))
-    df_split.columns = ['First Name', 'Last Name']
-
-    df_filtered['Formatted Date'] = pd.to_datetime(df_filtered.iloc[:, 1], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
-
-    df_result = pd.concat([df_split, df_filtered['Formatted Date']], axis=1)
-    df_result.to_csv(f'{output_folder}/cleaned_player_data.csv', index=False)
+def wrangle_data(output_folder: str):
+    df = pd.read_csv(f'{output_folder}/player_data.csv')
     
-process_task = PythonOperator(
-    task_id='process_csv_task',
-    python_callable=process_csv,
+    # Clean names and reassign to a new column
+    df['Name'] = df.iloc[:, 0].apply(clean_names)
+    
+    # Convert birthdates to standard format
+    df['Birthday'] = pd.to_datetime(df.iloc[:, 1], format='%m/%d/%Y').dt.strftime('%Y-%m-%d')
+    
+    # Split names into first and last name
+    df_split = df['Name'].apply(lambda x: pd.Series(split_names(x)))
+    df_split.columns = ['First Name', 'Last Name']
+    
+    # Combine cleaned data
+    df_wrangled = pd.concat([df_split, df['Birthday']], axis=1)
+    df_wrangled.to_csv(f'{output_folder}/player_data.csv', index=False)
+
+wrangle_data = PythonOperator(
+    task_id='wrangle_data_task',
+    python_callable=wrangle_data,
     op_kwargs={"output_folder": "/opt/airflow/data"},
     dag=dag,
 )
@@ -85,8 +102,7 @@ connection_check = BranchPythonOperator(
 )
 
 def call_api_online(output_folder: str):
-    # Opening the players file
-    with open(f'{output_folder}/cleaned_player_data.csv', 'r') as file: 
+    with open(f'{output_folder}/player_data.csv', 'r') as file: 
         
         #Reading player information line by line
         for line in file:
@@ -133,7 +149,7 @@ def call_api_online(output_folder: str):
             except:
                 print("Issue with value of the current player, skipping")
 
-enriching_task_online = PythonOperator(
+enrich_data_online = PythonOperator(
     task_id='online_source',
     python_callable=call_api_online,
     dag=dag,
@@ -141,7 +157,7 @@ enriching_task_online = PythonOperator(
     trigger_rule='all_success',
 )
 
-enriching_task_offline = BashOperator(
+enrich_data_offline = BashOperator(
     task_id='offline_source',
     bash_command='cp /opt/airflow/data/offline_movie_data/* /opt/airflow/data/movie_data/',
     dag=dag,
@@ -153,4 +169,5 @@ end = DummyOperator(
     trigger_rule='none_failed'
 )
 
-process_task >> delete_task >> connection_check >> [enriching_task_online, enriching_task_offline] >> end
+clean_data >> wrangle_data >> delete_task >> connection_check 
+connection_check >> [enrich_data_online, enrich_data_offline] >> end
